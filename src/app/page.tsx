01d94c1403d8e0f0762c654196829fc3d503b2d9
Muffin Mousies <foxdale.axelrod@gmail.com>
2023-11-20 16:34:59 -0800

    add naked logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,12 +32,12 @@ export default function Home() {
         <div>
           Hey, we think you're pretty rad, but you have to be 18+ to visit this
           site. This is because it includes graphic depictions of gay mice
-          having &#128293;hot&#128293; gay sex. For example, it includes a
-          sequence of images where one gay mouse conducts his penis into another
-          gay mouse's anus and proceeds to perform sexual intercourse until he
-          has an orgasm and ejaculates warm semen into the other mouse's rectum.
-          If you are under 18 or uncomfortable with such content during the
-          Biden era, then we must ask you to depart from this website.
+          having gay mouse sex. For example, it includes a sequence of images
+          where one gay mouse conducts his penis into another gay mouse's anus
+          and proceeds to perform sexual intercourse until he has an orgasm and
+          ejaculates warm semen into the other mouse's rectum. If you are under
+          18 or uncomfortable with such content during the Biden era, then we
+          must ask you to depart from this website.
         </div>
         <div className="items-center text-xl pt-12">Are you 18?</div>
         <button
@@ -80,7 +80,7 @@ export default function Home() {
   } else {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-12">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex lg:pb-0 pb-12">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             {blurb}
           </p>
@@ -101,23 +101,23 @@ export default function Home() {
 
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            className="relative dark:drop-shadow-[0_0_0.9rem_#ffffff] dark:opacity-90 dark:contrast-125 dark:backdrop-sepia"
+            src={naked ? '/homepage-1-n.png' : '/homepage-1.png'}
+            alt="mousies"
+            width={800}
+            height={800}
             priority
           />
         </div>
 
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
-            href="mousies"
+            href="gallery"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>Mousies</h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Get to know the mousies and their friends!
+            <h2 className={`mb-3 text-2xl font-semibold`}>Gallery</h2>
+            <p className={`m-auto lg:m-0 max-w-[30ch] text-sm opacity-50`}>
+              A fun little photo album
             </p>
           </a>
 
@@ -126,8 +126,8 @@ export default function Home() {
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>Read</h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Comics and stories about love and pasteries.
+            <p className={`m-auto lg:m-0 max-w-[30ch] text-sm opacity-50`}>
+              Comics and stories about love and pasteries
             </p>
           </a>
 
@@ -138,9 +138,9 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-2xl font-semibold`}>Subscribe Star</h2>
-            <span className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Financially support the mousies and receive exclusive content!
-            </span>
+            <p className={`m-auto lg:m-0 max-w-[30ch] text-sm opacity-50`}>
+              Support the mousies and receive exclusive content!
+            </p>
           </a>
 
           <div
@@ -150,7 +150,7 @@ export default function Home() {
             {!naked && (
               <>
                 <h2 className={`mb-3 text-2xl font-semibold`}>Naked!</h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                <p className={`m-auto lg:m-0 max-w-[30ch] text-sm opacity-50`}>
                   Click for a surprise!
                 </p>
               </>
@@ -158,7 +158,7 @@ export default function Home() {
             {naked && (
               <>
                 <h2 className={`mb-3 text-2xl font-semibold`}>Put it back!</h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                <p className={`m-auto lg:m-0 max-w-[30ch] text-sm opacity-50`}>
                   Aaaaaaaahhhhhh!!!!!
                 </p>
               </>
